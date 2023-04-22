125c33cf3f590585ab86b4fa5b195cd0a5954eae
omaryesithvilladiego <omaryesithvilladiegocarrascal@outlook.com>
2023-04-22 12:22:46 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className='container'>
+     <div className='main-content'>
+      <div className='content-image'>
+      <img className='main-image' src="/images/image-qr-code.png" alt="image" />
+      </div>
+      <div className="content-text">
+      <center><h1 className='title'>Improve your Front End Skills by building projects</h1></center>
+      <center><p className='paragraph'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id ducimus quisquam incidunt! Expedita molestias </p></center>
+      </div>
+      
+     </div>
+    </div>
+  )
+}
+
+export default App
